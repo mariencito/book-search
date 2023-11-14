3ea2f4aaf257d288b36e5ca040feae1589d63cda
marien <mariencastellanosjr@gmail.com>
2023-11-13 23:13:25 -0500

    saving work

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,47 @@
+const typeDefs = `
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    savedBooks: [Book]!
+  }
+
+  type Book {
+    authors: [String]
+    thoughtText: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+   
+    me: User
+  }
+
+  book BookInput{
+    authors: [String]
+    thoughtText: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(book: BookInput!): User
+    removeBook(bookId: String!): User
+   
+  }
+`;
+
+module.exports = typeDefs;
